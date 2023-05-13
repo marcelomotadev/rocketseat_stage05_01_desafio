2ed8f1530aa22e82108955785d66f76e79264e0f
Marcelo Mota <101136833+marcelomotadev@users.noreply.github.com>
2023-05-13 11:40:21 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function iniciarAnimacao(imagem) {
+  imagem.classList.add('imagem-vibrante');
+  imagem.classList.add('vibrando');
+}
+
+function pararAnimacao(imagem) {
+  imagem.classList.remove('imagem-vibrante');
+  imagem.classList.remove('vibrando');
+}
