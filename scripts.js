cde2080cfde0c05ed382a0855bb834ae24cc0274
Marcelo Mota <101136833+marcelomotadev@users.noreply.github.com>
2023-05-13 22:42:04 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,3 +1,27 @@
+const screen1 = document.querySelector(".screen1")
+const screen2 = document.querySelector(".screen2")
+const cookie = document.querySelector(".screen1 img")
+const resetButton = document.querySelector(".screen2 button")
+
+cookie.addEventListener('click', screenSwap)
+resetButton.addEventListener('click', screenSwap)
+
+
+
+
+
+
+
+
+
+
+// Funções
+
+function screenSwap() {
+  screen1.classList.toggle('hide')
+  screen2.classList.toggle('hide')
+}
+
 function iniciarAnimacao(imagem) {
   imagem.classList.add('imagem-vibrante');
   imagem.classList.add('vibrando');
