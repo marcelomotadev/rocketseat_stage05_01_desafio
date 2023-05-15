fb6f7e916800940d063e3181b502ddddcfff1e40
Marcelo Mota <101136833+marcelomotadev@users.noreply.github.com>
2023-05-14 22:26:09 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,21 +1,26 @@
 const screen1 = document.querySelector(".screen1")
 const screen2 = document.querySelector(".screen2")
+const luckPhrase = screen2.querySelector("h2")
 const cookie = document.querySelector(".screen1 img")
 const resetButton = document.querySelector(".screen2 button")
+let randomNumber
 
-cookie.addEventListener('click', screenSwap)
+cookie.addEventListener('click', chooseLuck)
 resetButton.addEventListener('click', screenSwap)
 
 
-
-
-
-
-
-
-
+let luckPhrases = ["A vida trará coisas boas se tiver paciência.","Demonstre amor e alegria em todas as oportunidades e verá que a paz nasce dentro de si.","Não compense na ira o que lhe falta na razão.","Defeitos e virtudes são apenas dois lados da mesma moeda.","A maior de todas as torres começa no solo.","Não há que ser forte. Há que ser flexível.","Se alguém está tão cansado que não possa te dar um sorriso,deixa-lhe o teu.","Todos os dias organiza os seus cabelos, por que não faz o mesmo com o coração?","Há três coisas que jamais voltam; a flecha lançada, a palavra dita e a oportunidade perdida.","A juventude não é uma época da vida, é um estado de espírito.","Podemos escolher o que semear, mas somos obrigados a colher o que plantamos."]
 
 // Funções
+
+function chooseLuck() {
+  randomNumber = Math.round(Math.random() * 10)
+  let phraseTemp = luckPhrases[randomNumber]
+
+  luckPhrase.innerText = phraseTemp
+
+  screenSwap()
+}
 
 function screenSwap() {
   screen1.classList.toggle('hide')
